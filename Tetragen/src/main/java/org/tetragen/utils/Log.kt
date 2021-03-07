733ee12ceb96da53cbde47ba54f81c215c4d2d21
croissant396 <73412431+lelfries@users.noreply.github.com>
2021-03-07 10:35:26 -0600

    Moved one-time use classes into object Log
    Also added a function in Log that redirects to LogLevel

--- Tetragen/src/main/java/org/tetragen/utils/Log.kt
+++ Tetragen/src/main/java/org/tetragen/utils/Log.kt
@@ -63,69 +63,72 @@ object Log {
      * @param message The message to be sent.  */
     @JvmStatic
     fun finest(message: String) = logger.finest(message)
-}
+    
+    @JvmStatic
+    fun getLevelByName(level: String) = LogLevel.getLevelByName(level)
 
-/** A Class that represents Levels, which can be used in unison with [Log] */
-class LogLevel private constructor(private val level: Level) {
-    companion object {
-        /** A [LogLevel] object representing [Level.INFO]*/
-        @JvmStatic
-        val INFO = LogLevel(Level.INFO)
+    /** A Class that represents Levels, which can be used in unison with [Log] */
+    class LogLevel private constructor(private val level: Level) {
+        companion object {
+            /** A [LogLevel] object representing [Level.INFO]*/
+            @JvmStatic
+            val INFO = LogLevel(Level.INFO)
 
-        /** A [LogLevel] object representing [Level.WARNING]*/
-        @JvmStatic
-        val WARNING = LogLevel(Level.WARNING)
+            /** A [LogLevel] object representing [Level.WARNING]*/
+            @JvmStatic
+            val WARNING = LogLevel(Level.WARNING)
 
-        /** A [LogLevel] object representing [Level.SEVERE]*/
-        @JvmStatic
-        val SEVERE = LogLevel(Level.SEVERE)
+            /** A [LogLevel] object representing [Level.SEVERE]*/
+            @JvmStatic
+            val SEVERE = LogLevel(Level.SEVERE)
 
-        /** A [LogLevel] object representing [Level.CONFIG]*/
-        @JvmStatic
-        val CONFIG = LogLevel(Level.CONFIG)
+            /** A [LogLevel] object representing [Level.CONFIG]*/
+            @JvmStatic
+            val CONFIG = LogLevel(Level.CONFIG)
 
-        /** A [LogLevel] object representing [Level.FINE]*/
-        @JvmStatic
-        val FINE = LogLevel(Level.FINE)
+            /** A [LogLevel] object representing [Level.FINE]*/
+            @JvmStatic
+            val FINE = LogLevel(Level.FINE)
 
-        /** A [LogLevel] object representing [Level.FINER]*/
-        @JvmStatic
-        val FINER = LogLevel(Level.FINER)
+            /** A [LogLevel] object representing [Level.FINER]*/
+            @JvmStatic
+            val FINER = LogLevel(Level.FINER)
 
-        /** A [LogLevel] object representing [Level.FINEST]*/
-        @JvmStatic
-        val FINEST = LogLevel(Level.FINEST)
+            /** A [LogLevel] object representing [Level.FINEST]*/
+            @JvmStatic
+            val FINEST = LogLevel(Level.FINEST)
 
-        /** Returns the [LogLevel] that has the name given.
-         * @param level A [String] that represents a [LogLevel].
-         * @return A [LogLevel] that has the same name as [level]. (Provided there is one.)
-         * @exception IllegalLevelException If there is no such [LogLevel]. */
-        @JvmStatic
-        fun getLevelByName(level: String): LogLevel {
-            return when (level.toLowerCase()) {
-                "info" -> INFO
-                "warn" -> WARNING
-                "warning" -> WARNING
-                "severe" -> SEVERE
-                "conf" -> CONFIG
-                "config" -> CONFIG
-                "fine" -> FINE
-                "finer" -> FINER
-                "finest" -> FINEST
-                else -> throw IllegalLevelException("$level is not a valid level!")
+            /** Returns the [LogLevel] that has the name given.
+             * @param level A [String] that represents a [LogLevel].
+             * @return A [LogLevel] that has the same name as [level]. (Provided there is one.)
+             * @exception IllegalLevelException If there is no such [LogLevel]. */
+            @JvmStatic
+            fun getLevelByName(level: String): LogLevel {
+                return when (level.toLowerCase()) {
+                    "info" -> INFO
+                    "warn" -> WARNING
+                    "warning" -> WARNING
+                    "severe" -> SEVERE
+                    "conf" -> CONFIG
+                    "config" -> CONFIG
+                    "fine" -> FINE
+                    "finer" -> FINER
+                    "finest" -> FINEST
+                    else -> throw IllegalLevelException("$level is not a valid level!")
+                }
             }
         }
+
+        /** Returns the [Level] that this object represents.
+         * @return The [Level] that this object represents. */
+        internal fun getLoggingLevel() = level
+
+        /** Returns the name of this LogLevel*/
+        override fun toString(): String = "LogLevel: ${level.name.toLowerCase()}"
     }
 
-    /** Returns the [Level] that this object represents.
-     * @return The [Level] that this object represents. */
-    internal fun getLoggingLevel() = level
-
-    /** Returns the name of this LogLevel*/
-    override fun toString(): String = "LogLevel: ${level.name.toLowerCase()}"
+    /** An Exception that is thrown when a specified [LogLevel] does not exist. */
+    class IllegalLevelException // This syntax is pretty ... interesting.
+    /** The constructor for this class (Provided that there is only one time when this class is used.)*/
+        (message: String) : IllegalArgumentException(message)
 }
-
-/** An Exception that is thrown when a specified [LogLevel] does not exist. */
-class IllegalLevelException // This syntax is pretty ... interesting.
-/** The constructor for this class (Provided that there is only one time when this class is used.)*/
-    (message: String) : IllegalArgumentException(message)
